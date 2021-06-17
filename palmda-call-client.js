9753766f2c7d5ba5752101b8aeb546673b020bba
destan <destan@dorukdestan.com>
2021-06-17 17:06:29 +0300

    error codes

--- palmda-call-client.js
+++ palmda-call-client.js
@@ -1,24 +1,29 @@
 {// ================= Call Plugin Library =================
-  
-  if(typeof PalmdaClient !== "undefined") {
-    throw new Error('PalmdaClient is not defined. Call Plugin Library should be added to the page after PalmdaClient is defined.');//TODO give an error code
+  const CALL_VERSION = "0.1.0";
+  if (typeof PalmdaClient !== "undefined") {
+    throw new Error('E6 PalmdaClient is not defined. Call Plugin Library should be added to the page after PalmdaClient is defined.');
   }
 
   // All phone numbers are in E164 format (+905051234567)
   function init() {
     PalmdaClient.prototype.incomingCall = function (phoneNumber) {
-        sendMessage({ id: 'pluginId', type: 'incomingCall', auth: 'token', data: { phoneNumber: phoneNumber } });
+      sendMessage({id: 'pluginId', type: 'incomingCall', auth: 'token', data: {phoneNumber: phoneNumber}});
     };
 
     PalmdaClient.prototype.answeredCall = function (phoneNumber) {
-        sendMessage({ id: 'pluginId', type: 'answeredCall', auth: 'token', data: { phoneNumber: phoneNumber } });
+      sendMessage({id: 'pluginId', type: 'answeredCall', auth: 'token', data: {phoneNumber: phoneNumber}});
     };
 
     PalmdaClient.prototype.endedCall = function (phoneNumber) {
-        sendMessage({ id: 'pluginId', type: 'endedCall', auth: 'token', data: { phoneNumber: phoneNumber } });
+      sendMessage({id: 'pluginId', type: 'endedCall', auth: 'token', data: {phoneNumber: phoneNumber}});
     };
+
+    Object.defineProperty(PalmdaClient.instance(), 'callVersion', {
+      value: CALL_VERSION,
+      writable: false
+    });
   }
-  
+
 }// end of client library
 
 /*
--- palmda-client.js
+++ palmda-client.js
@@ -1,100 +1,125 @@
 {
 // ------------------ CLIENT LIBRARY ----------------------
-const VERSION = "0.1.0";
-let initializing = false;
-let initMethodCalled = false;
-let instance = null;
-let pluginConfig = null;
+  const VERSION = "0.1.0";
 
-function sendMessage(data) {
+  if (window.PalmdaClient !== "undefined") {
+    throw new Error(`E0 PalmdaClient is already defined. Existing version: '${window.PalmdaClient.version}' and this version: '${VERSION}'.`);
+  }
+
+  const origin = window.location.hash.replace('#origin=', '');
+  if (!origin) {
+    throw new Error('E1 Origin is empty!');
+  }
+
+  let initializing = false;
+  let initMethodCalled = false;
+  let instance = null;
+  let pluginConfig = null;
+
+  function sendMessage(data) {
     window.parent.postMessage(data, origin);
-}
+  }
 
-class PalmdaClient {
+  class PalmdaClient {
     constructor() {
-        if (!initializing) {
-            throw new Error('The constructor is private, please use instance() method.');
-        }
-        initializing = false;
+      if (!initializing) {
+        throw new Error('E2 The constructor is private, please use instance() method.');
+      }
+      initializing = false;
 
-        if (!instance) {
-            instance = this;
-        }
-        
-        Object.defineProperty(instance, 'version', {
-          value: VERSION,
-          writable: false
-        });
+      if (!instance) {
+        instance = this;
+      }
 
-        return instance;
+      Object.defineProperty(instance, 'version', {
+        value: VERSION,
+        writable: false
+      });
+
+      return instance;
     }
 
     static instance() {
-        initializing = true;
-        return new PalmdaClient();
+      initializing = true;
+      return new PalmdaClient();
     }
 
     /**
-    * Internal usage, don't use this method.
-    * This must be the first method ever called on this object. This method is called by the library itself and users should not call this method.
-    */
+     * Internal usage, don't use this method.
+     * This must be the first method ever called on this object. This method is called by the library itself and users should not call this method.
+     */
     _init() {
-        if (pluginConfig) {
-            return Promise.resolve(pluginConfig);
-        }
+      if (pluginConfig) {
+        return Promise.resolve(pluginConfig);
+      }
 
-        if (initMethodCalled) {
-            return new Promise((resolve, reject) => {
-                const intervalHandle = setInterval(() => {
-                    if (pluginConfig != null) {
-                        clearInterval(intervalHandle);
-                        resolve(pluginConfig);
-                    }
-                }, 50);
-            });
-        }
-        initMethodCalled = true;
-
-        return new Promise((resolutionFunc, rejectionFunc) => {
-            window.addEventListener(
-                'message',
-                (e) => {
-                    if (e.origin !== origin) return;
-
-                    if (!e.data || !e.data.type) return;
-
-                    switch (e.data.type) {
-                        case 'init': {
-                            pluginConfig = e.data.data;
-                            resolutionFunc(pluginConfig);
-                            return;
-                        }
-                        default: {
-                            const type = e.data.type;
-
-                            return;
-                        }
-                    }
-                },
-                false,
-            );
-            sendMessage({
-                id: 'pluginId',
-                type: 'init',
-                auth: 'token',
-                note: 'Send me the pluginConfig data to connect WS',
-            });
+      if (initMethodCalled) {
+        return new Promise((resolve, reject) => {
+          //TODO do we need a wait timeout?
+          const intervalHandle = setInterval(() => {
+            if (pluginConfig != null) {
+              clearInterval(intervalHandle);
+              resolve(pluginConfig);
+            }
+          }, 50);
         });
+      }
+      initMethodCalled = true;
+
+      return new Promise((resolve, reject) => {
+        window.addEventListener(
+          'message',
+          (e) => {
+            if (e.origin !== origin) {
+              console.error(`Origins does not match. Expected '${origin}' but found '${e.origin}'!`);
+              reject(new Error(`E3 Origins does not match. Expected '${origin}' but found '${e.origin}'!`));
+              return;
+            }
+
+            if (!e.data) {
+              console.error(`Event data (event.data) is missing.`, e);
+              reject(new Error(`E4 Event data (event.data) is missing.`));
+              return;
+            }
+
+            if (!e.data.type) {
+              console.error(`Event data type (event.data.type) is missing.`, e);
+              reject(new Error(`E5 Event data type (event.data.type) is missing.`));
+              return;
+            }
+
+            switch (e.data.type) {
+              case 'init': {
+                pluginConfig = e.data.data;
+                resolve(pluginConfig);
+                return;
+              }
+              default: {
+                const type = e.data.type;
+
+                return;
+              }
+            }
+          },
+          false,
+        );
+        sendMessage({
+          id: 'pluginId',//FIXME
+          type: 'init',
+          auth: 'token',
+          note: 'Send me the pluginConfig data to connect WS',
+        });
+      });
     }
 
     getConfig() {
-        return this._init();
+      return this._init();
     }
-}
+  }
 
-PalmdaClient.instance()
+  PalmdaClient.instance()
     ._init()
     .then((data) => (pluginConfig = data));
 
-window.PalmdaClient = PalmdaClient;
+  window.PalmdaClient = PalmdaClient;
 }
